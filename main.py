96c538e71f65b83da0bd76fde07f4a7378e201d9
Gita <geetabhutiya21@gmail.com>
2025-11-22 00:05:04 +0000

    US1: Add job implemented

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from job_unit_scheduler import JobUnitScheduler
 s=JobUnitScheduler()
 print("Welcome to Job Scheduler")
-print("\n")
+
 
 def show_menu():
-    print("\n===Job Scheduler Menu ===)")# manage your jobs
-    print("\n 1. Add Job (US1")
-    print("2. View All Jobs (US2")
-    print("10. Exit")
+    print("===Job Scheduler Menu ===)")# manage your jobs
+    print("1. Add Job (US1)")
+    print("0. Exit")
 
+while True:
+    show_menu()
+    print("\n")
+    choice=input("Enter your choice (1 or 10):- ")
 
-#US1: Add job
-print("1. Add a job (US1)")
-name = input("Enter job name: ")
-description = input("Enter job description: ")
-deadline = input("Enter job deadline (YYYY-MM-DD): ")
+    #US1: Add job
+    if choice=="1":
+         print("=>Add a job (US1)")
+         name = input("Enter job name: ")
+         description = input("Enter job description: ")
+         deadline = input("Enter job deadline (YYYY-MM-DD): ")
 
-job = s.add_job(name, description, deadline)
+         job = s.add_job(name, description, deadline)
 
-print(f"\nJob added successfully! Job ID: {job.id}")
+         print(f"\nJob added successfully! Job ID: {job.id}")
+    elif choice=="0":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
