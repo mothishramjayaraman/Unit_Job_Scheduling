97c03ff5900f8cef5d7b4a7a75bf8af9384331ba
Gita <geetabhutiya21@gmail.com>
2025-11-22 01:31:05 +0000

    US2: List jobs implemented

--- job_unit_scheduler.py
+++ job_unit_scheduler.py
@@ -23,6 +23,6 @@ class JobUnitScheduler:
         self.next_id += 1
         return job
 
-    #US2: View All Jobs
-    def view_all_jobs(self):
+    #US2: List All Jobs
+    def list_jobs(self):
         return self.jobs
